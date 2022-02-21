a718179e8e8b090b92599f8c1b817d029bc2cb99
Nicholas Clarke <nick@topos.org.uk>
2022-02-21 12:04:36 +0100

    WIP cardano testing

--- ouroboros-consensus-cardano-test/src/Test/ThreadNet/TxGen/Mary.hs
+++ ouroboros-consensus-cardano-test/src/Test/ThreadNet/TxGen/Mary.hs
@@ -5,13 +5,14 @@ module Test.ThreadNet.TxGen.Mary () where
 
 import           Ouroboros.Consensus.Shelley.Eras
 import           Ouroboros.Consensus.Shelley.Ledger
+import Ouroboros.Consensus.Protocol.TPraos (TPraos)
 
 import           Test.ThreadNet.TxGen (TxGen (..))
 
 -- | Dummy generator until CAD-2119 is done, i.e., the transaction generator in
 -- the ledger has been generalised over the eras.
-instance TxGen (ShelleyBlock (MaryEra c)) where
+instance TxGen (ShelleyBlock (TPraos c) (MaryEra c)) where
 
-  type TxGenExtra (ShelleyBlock (MaryEra c)) = ()
+  type TxGenExtra _ = ()
 
   testGenTxs _ _ _ _ _ _ = pure []
